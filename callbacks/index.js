d6e0158a3763668012ef99530c97d9ced35ef45b
C-137DTS <guillenfortich@gmail.com>
2020-09-19 12:32:18 -0500

    Haciendo peticiones a una API utilizando callbacks

--- callbacks/index.js
+++ callbacks/index.js
@@ -1,21 +1,35 @@
-function sum(num1, num2){
-    return num1 + num2
+const API = 'https://rickandmortyapi.com/api/character/'
+let character_id = parseInt(prompt('Digite el Id del personaje que deseas obtener: '))
+
+function fetchData(url_api, callback) {
+    let xhttp = new XMLHttpRequest()
+    xhttp.open('GET', url_api, true)
+    xhttp.onreadystatechange = event => {
+        if(xhttp.readyState === 4) {
+            if(xhttp.status === 200) {
+                callback(null, JSON.parse(xhttp.responseText))
+            } else {
+                const error =  new Error('Error ' + url_api)
+                callback(error, null)
+            }
+        } 
+    }
+    xhttp.send()
 }
 
-function calc(num1, num2, callback){
-    return callback(num1, num2)
-}
-
-print(calc(5, 3, sum))
-
-function date(callback) {
-    print(new Date)
-    setTimeout(() => {
-        let date = new Date
-        callback(date)
-    }, 3000)
-}
-
-function printDate(dateNow) {
-    print(dateNow)
-}
+fetchData(API + character_id, (error1, data1) => {
+    if(error1) return console.error(error1)
+    console.log(data1.name)
+    
+    if(data1.origin.url){
+        fetchData(data1.origin.url, (error2, data2) => {
+            if(error2) {
+                return console.error(error2)
+            }       
+            console.log(data2.dimension)
+        })
+    } else {
+        console.log('Aún no se conoce su dimensión')
+    }
+    
+})
