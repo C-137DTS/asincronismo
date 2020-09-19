af659fab69320e60001befc6044ec31c0004ea11
C-137DTS <guillenfortich@gmail.com>
2020-09-19 12:31:15 -0500

    Haciendo peticiones a una API utilizando promesas

---
+++ promesas/index.js
@@ -0,0 +1,30 @@
+const API = 'https://rickandmortyapi.com/api/character/'
+
+const fetchData = url_api => {
+    return new Promise((resolve, reject) => {
+        const xhttp = new XMLHttpRequest()
+        xhttp.open('GET', url_api, true)
+        xhttp.onreadystatechange = event => {
+            if(xhttp.readyState === 4) {
+                (xhttp.status === 200)
+                    ? resolve(JSON.parse(xhttp.responseText))
+                    : reject(new Error('Error: ', url_api)) 
+            } 
+        }
+        xhttp.send()
+    })
+}  
+
+fetchData(API)
+    .then(data => {
+        console.log(data.info.count)
+        return fetchData(`${API}${data.results[0].id}`)
+    })
+    .then(data => {
+        console.log(data.name)
+        return  fetchData(data.origin.url)
+    })
+    .then(data => {
+        console.log(data.dimension)
+    })
+    .catch(err => console.error(err))
